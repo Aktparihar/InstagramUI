e12266e3f62094a6fc13017371c7b5e070b7c53e
Aktparihar <ankitpariharmiku@gmail.com>
2020-12-03 11:39:41 +0530

    UI code

---
+++ app/src/main/java/com/example/instauicode/HomeActivity.java
@@ -0,0 +1,39 @@
+package com.example.instauicode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.example.instauicode.Utils.BottomNavigationViewHelper;
+import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
+public class HomeActivity extends AppCompatActivity {
+    private static final String TAG = "HomeActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: starting.");
+
+        setupBottomNavigationView();
+    }
+
+    /**
+     * BottomNavigationView setup
+     */
+    private void setupBottomNavigationView(){
+        Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
+        BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.bottomNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
+        BottomNavigationViewEx topNavigationViewEx = (BottomNavigationViewEx) findViewById(R.id.topNavViewBar);
+        BottomNavigationViewHelper.setupBottomNavigationView(topNavigationViewEx);
+    }
+
+}
